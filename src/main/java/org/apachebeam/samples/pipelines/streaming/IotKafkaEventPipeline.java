e503f7bf3910cc19a420327ec911ff58b9e80dd8
Jozimar Back <jozimar5@gmail.com>
2023-06-13 21:51:23 -0300

    kafka pipeline sample with filter and windowing

--- src/main/java/org/apachebeam/samples/pipelines/streaming/IotKafkaEventPipeline.java
+++ src/main/java/org/apachebeam/samples/pipelines/streaming/IotKafkaEventPipeline.java
@@ -2,10 +2,15 @@ package org.apachebeam.samples.pipelines.streaming;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
 import org.apache.kafka.common.serialization.LongDeserializer;
+import org.joda.time.Duration;
 
 public class IotKafkaEventPipeline {
     public static void main(String[] args) {
@@ -19,12 +24,21 @@ public class IotKafkaEventPipeline {
                 .withoutMetadata()
         )
                 .apply(Values.<IotEvent>create())
-                .apply(ParDo.of(new DoFn<IotEvent, Void>() {
+                .apply(Window.<IotEvent>into(FixedWindows.of(Duration.standardSeconds(10))))
+                .apply(ParDo.of(new DoFn<IotEvent, String>() {
                     public void processElement(ProcessContext c){
-                        System.out.println(c.element().getDeviceId());
+                        if(c.element().getTemperature()>80.0){
+                            c.output(c.element().getDeviceId());
+                        }
                     }
                 }))
-        ;
+                .apply(Count.perElement())
+                .apply(ParDo.of(new DoFn<KV<String, Long>, Void>() {
+                   @ProcessElement
+                   public void processElement(ProcessContext c){
+                       System.out.println(c.element());
+                   }
+                }));
 
         p.run();
     }
